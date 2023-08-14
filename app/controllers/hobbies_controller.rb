65ca3aac6b83cb61cda6258bb1ad8b669d6b5a62
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-14 17:35:56 +0900

    趣味投稿機能実装

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -2,11 +2,24 @@ class HobbiesController < ApplicationController
   def index
   end
 
+  def new
+    @hobby = Hobby.new
+  end
+
+  def create
+    @hobby = Hobby.new(hobby_params)
+    if @hobby.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+     end
+  end
+  
 
 
   private
 
   def hobby_params
-    params.require(:hobby).permit(:genre_id, :activity, :time).merge(user_id: current_user.id)
+    params.require(:hobby).permit(:genre_id, :activity, :time, :date).merge(user_id: current_user.id)
   end
 end
--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,6 +1,7 @@
 class Hobby < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
   belongs_to :user
 
-  validates :time, :activity, :date, presence: true
-  validates :genre_id, presence: true, numericality: { other_than: 1, message: "を選択してください" } 
+  validates :time, :activity, :date, :genre_id, presence: true
 end
