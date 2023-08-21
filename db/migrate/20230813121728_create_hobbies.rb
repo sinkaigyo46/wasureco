8a65abd597123977b3811ba77c50197d518241fe
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-21 17:11:47 +0900

    ビューの調整

--- db/migrate/20230813121728_create_hobbies.rb
+++ db/migrate/20230813121728_create_hobbies.rb
@@ -4,7 +4,7 @@ class CreateHobbies < ActiveRecord::Migration[7.0]
       t.integer        :genre_id,     null: false 
       t.date           :date,         null: false
       t.text           :activity,     null: false 
-      t.time           :time,         null: false 
+      t.float          :time,         null: false 
       t.references     :user,         null: false,  foreign_key: true 
       t.timestamps
     end
