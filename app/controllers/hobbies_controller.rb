85b7fd30d5fd64b3aea3e6f4faa67542d0a2deb6
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-24 20:52:46 +0900

    デプロイエラーの解消

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -51,11 +51,15 @@ class HobbiesController < ApplicationController
 
   def calculate_genre_total_times_by_month(hobbies)
     genre_total_times_by_month = Hash.new { |hash, key| hash[key] = Hash.new(0) }
-
+  
     hobbies.each do |hobby|
-      genre_total_times_by_month[hobby.date.month][hobby.genre_id] += hobby.time
+      month = hobby.date.month
+      genre_id = hobby.genre_id
+      time = hobby.time.to_i  
+  
+      genre_total_times_by_month[month][genre_id] += time
     end
-
+  
     genre_total_times_by_month
   end
 end
