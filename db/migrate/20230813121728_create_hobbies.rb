00a02e1b8276e7f3a12f4b80118d683e716d69e9
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-24 21:15:12 +0900

    DB変更

--- db/migrate/20230813121728_create_hobbies.rb
+++ db/migrate/20230813121728_create_hobbies.rb
@@ -3,8 +3,8 @@ class CreateHobbies < ActiveRecord::Migration[7.0]
     create_table :hobbies do |t|
       t.integer        :genre_id,     null: false 
       t.date           :date,         null: false
-      t.text           :activity,     null: false 
       t.float          :time,         null: false 
+      t.text           :activity,     null: false 
       t.references     :user,         null: false,  foreign_key: true 
       t.timestamps
     end
