1e70e79eb4eb05e2bebaa50d8ce0e435bb9d9827
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-28 18:30:04 +0900

    趣味投稿機能テスト実行

--- spec/models/hobby_spec.rb
+++ spec/models/hobby_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Hobby, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @hobby = FactoryBot.build(:hobby)
+  end
+
+  describe '趣味投稿' do
+    context '趣味投稿ができる場合' do
+      it '全ての項目が出力されていれば保存できる' do
+        expect(@hobby).to be_valid
+      end
+      it 'コメントが空でも投稿できる' do
+        @hobby.activity = ''
+        expect(@hobby).to be_valid
+      end
+    end
+
+    context '趣味投稿ができない場合' do
+      it '日付が空では登録できない' do
+        @hobby.date = ''
+        @hobby.valid?
+        expect(@hobby.errors.full_messages).to include('日付を入力してください')
+      end
+      it 'ジャンルが空では登録できない' do
+        @hobby.genre_id = ''
+        @hobby.valid?
+        expect(@hobby.errors.full_messages).to include('ジャンルを入力してください')
+      end
+      it '時間が空では登録できない' do
+        @hobby.time = ''
+        @hobby.valid?
+        expect(@hobby.errors.full_messages).to include('時間を入力してください')
+      end
+      it 'ユーザーが紐付いていないと登録できない' do
+        @hobby.user = nil
+        @hobby.valid?
+        expect(@hobby.errors.full_messages).to include('Userを入力してください')
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,27 +7,27 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+      it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
     context '新規登録できない場合' do
-      it "nameが空では登録できない" do
+      it 'nameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
+        expect(@user.errors.full_messages).to include('ニックネームを入力してください')
       end
-      it "emailが空では登録できない" do
+      it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Eメールを入力してください")
+        expect(@user.errors.full_messages).to include('Eメールを入力してください')
       end
-      it "passwordが空では登録できない" do
+      it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("パスワードを入力してください")
+        expect(@user.errors.full_messages).to include('パスワードを入力してください')
       end
-			it 'passwordが5文字以下では登録できない' do
+      it 'passwordが5文字以下では登録できない' do
         @user.password = '12345'
         @user.password_confirmation = '12345'
         @user.valid?
@@ -35,8 +35,8 @@ RSpec.describe User, type: :model do
       end
 
       it 'passwordが129文字以上では登録できない' do
-        @user.password =  Faker::Internet.password(min_length: 129, max_length: 150)
-        @user.password_confirmation =  @user.password
+        @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include('パスワードは128文字以内で入力してください')
       end
@@ -45,16 +45,16 @@ RSpec.describe User, type: :model do
         @user.password = '123456'
         @user.password_confirmation = '1234567'
         @user.valid?
-        expect(@user.errors.full_messages).to include("パスワード（再）とパスワードの入力が一致しません")
-			end
-			it '重複したemailが存在する場合は登録できない' do
+        expect(@user.errors.full_messages).to include('パスワード（再）とパスワードの入力が一致しません')
+      end
+      it '重複したemailが存在する場合は登録できない' do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
       end
 
-     it 'emailは@を含まないと登録できない' do
+      it 'emailは@を含まないと登録できない' do
         @user.email = 'testmail'
         @user.valid?
         expect(@user.errors.full_messages).to include('Eメールは不正な値です')
