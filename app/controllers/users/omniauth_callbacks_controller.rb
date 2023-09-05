83cf61387273a385b2ca9175c20573c57dfd4621
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-09-05 16:14:39 +0900

    Revert "編集機能実装"
    This reverts commit b0a3cfb9c24cd3a95816340d87d00f6c6550f1d3.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,15 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    sns_info = User.from_omniauth(request.env['omniauth.auth'])
+  
+   def google_oauth2
+    sns_info = User.from_omniauth(request.env["omniauth.auth"])
     @user = sns_info[:user]
 
-    if @user.persisted?
+     if @user.persisted? 
       sign_in_and_redirect @user, event: :authentication
-    else
+    else 
       @sns_id = sns_info[:sns].id
       render template: 'devise/registrations/new'
     end
-  end
+   end
+  
 end
