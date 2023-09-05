b0a3cfb9c24cd3a95816340d87d00f6c6550f1d3
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-09-05 16:01:33 +0900

    編集機能実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,14 +10,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-   def create
+  def create
     if params[:sns_auth] == 'true'
       pass = Devise.friendly_token
       params[:user][:password] = pass
       params[:user][:password_confirmation] = pass
     end
     super
-   end
+  end
 
   # GET /resource/edit
   # def edit
